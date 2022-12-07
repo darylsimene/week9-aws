74dfe1ed09797020b9387396a4dc21d1a9ba6135
darylsimene <john.simene@supportzebra.com>
2022-12-07 09:13:50 +0800

    aws questions

---
+++ week9day1.js
@@ -0,0 +1,112 @@
+//! What is AWS EC2?
+// AWS EC2 is a service which is on the AWS platfor. It is allows developers to avail instances of what computing devices could do. but do it virtually. It is scalable, meaning users can scale up when they need to and then scale down if they need to.
+
+//! What are some use cases of using AWS EC2?
+// Some use cases of using AWS EC2 is when you know that the your site is gonna have a lot of traffic that day/month, and you wish to relieve and possibly avoid users from inconvenience. You will avail an instance so that you can have as many servers as you want. And when the traffic lessens, then you can choose to dismantle the virtual device.
+
+//! What is AWS Lambda?
+//AWS Lambda is also a service in AWS wherein you will run code without provisioning or managing insfrastructures which are prevalent when your servers are done traditionally.
+
+//! What are some uses case of using AWS Lambda?
+// One use cases of AWS Lambda is when you want to deploy your code, you have to ready your code and the code only runs when you trigger. And you can reduce cost if the code run.
+
+//! How does AWS Lambda work for:
+//? a. File Processing
+//In File processing , lambda runs as it gets triggers right after a file upload was put in the Amazon S3 bucket.
+//? b. Web Applications
+//When user interacts with the UI of the web application, and API gateway has been made, lambda is triggered to perform a function wherein it gets json data from the dynamoDB.
+//? c. Stream Processing
+//Stream processing is done as amazon kinesis is used, lambda is triggered to generate hashtag trend data that is stored in dynamoDB. the data stored is available to the users to query.
+
+//! What is S3?
+//S3 is a object storage service in AWS. It is kinda like a database, but it is only relevant in file storing data. Users can store and protect any amount of data virtually.
+
+//! How does S3 work?
+// S3 works with customer of all sizes and industries to store and protect any amount of data for virtually any use case like data lakes, cloud-native application , and also for mobile apps.
+
+//! What are some use cases of S3?
+//Some uses cases of S3 is for file uploads of videos and pictures  and data(from analytics and application data, and to backups and archive)
+
+//! What is DynamoDB
+//DynamoDB allows users to create a NoSql database which is scalable, capable of fast storing and retrieving users without causing much of a traffic.
+
+//! How does DynamoDB work?
+//DynamoDB works by offloading the administrative burdens of operating and scaling a distributed database. It is fast, a performance that is much more predictable, and a seamless scalability
+
+//!What are some use cases of DynamoDB?
+//DynamoDB can be used when you want a scalable database and when you want storage to be build virtually. It also eliminates the operational burden and complexity involved in protecting sensitive data by providing encryption at REST.
+
+//! What is Aurora?
+// Amazon Aurora is a relational database management system offered in Amazon.  It is built in the cloud with full compatability of MySQL and PostgreSQL compatability.
+
+//! How does Aurora work?
+// Aurora has a high-performance storage subsystem and the underlying storage expands automatically. Aurora also can automate and standardizes database clustering and replication, which is hard to do in database deployment and maintenance.
+
+//! What are some use cases of Aurora?
+// Some use cases of Aurora is for when your data's are value-based. It is also use when you're existing frameworks before turning into AWS is stil in MySQL/PostgreSQL.
+
+//! What is AWS VPC?
+// AWS VPC or virtual private cloud provides users with full control over the virtual networking environmment, including resource placement, connectivity, and security.
+
+//! How does AWS VPC work?
+// This virtual network has the advantages of using the scalable infrastructure of AWS while closely resembling a conventional network that you would operate in your own data center.
+
+//! What are some use cases of VPC?
+// Some uses cases of using virtual private cloud if for you to networking administrator stuffs and add subnets.
+
+//! What is API Gateway?
+// In order to construct, publish, maintain, monitor, and protect APIs at any size, developers may use the fully managed service known as Amazon API Gateway. Applications use APIs as their "front door" to data, business logic, or functionality in your backend services.
+
+//! How does API Gateway work?
+// API gateway creates RESTful APIs that are HTTP based, it enables stateless clien-server communication, and implement standard HTTP methods such as GET, POST, PUT, PATCH, and DELETE
+
+//! What are some use cases of API Gateway?
+// API gateways are use by IoT devices, web and mobile applications, connected users and streaming dashboards. API gateway
+
+//! 21. What is CloudFront?
+// Excellent performance, security, and developer friendliness are the main priorities of the content delivery network (CDN) service Amazon CloudFront.
+
+//! 22. How does CloudFront work?
+// Amazon cloudfront is responsible in making the users of website/IoT devices speeds up the distribution of your web content and your image files. When there is cloudfront in your AWS, it helps with delivering your contents throughout the world's data centers which are called edge locations. When users are accessing your sites, the request the user wants is routed to the edge locations where there is less latency.
+
+//! 23. What are some use cases of CloudFront?
+// Best use cases of cloudfront is when you want users to experience the most convenient way possible in accessing your web services. In this way, you can provide users with less wait time, which in turn, turns into profit, because time is money.
+
+//! 24. What is Cognito?
+// Amazon cognito is a user identity and data synchronization service that makes it easy for developers to manage users from your application across multiple devices of the users/customers.
+
+//! 25. How does Cognito work?
+// Amazon Cognito works by helping you integrate user sign-in and signup, and you could also sign in by using your FB, Google, and other popular site accounts. It can also help you sync user data so that you can let users jump between different devices with ease.
+
+//! 26. What are some use cases of Cognito?
+// The use case of Cognito is for you to manage user sign-ups and sign-in, with the inclusion of letting the users of your application use third-party accounts from popular websites. It can also help if developers have their applications on multiple devices because Cognito helps with syncing.
+
+//! 27. What is CloudWatch?
+// Cloudwatch is a monitoring and management service built for developers, system operators, engineers, and IT managers. It provides data and actionable insights to monitor your application.
+
+//! 28. How does CloudWatch work?
+// It analyzes and respond when your application will have system-wide performance change, and it optimizes resource utilization.
+
+//! 29. What are some use cases of CloudWatch?
+// The use case of Cloudwatch is its ability to record the performance that happens in your system-wide change. It helps you track and monitor the performance of your application.
+
+//! 30. What is Secrets Manager?
+// It is protects the secrets that are neeeded to access your application, services, and IT services.
+
+//! 31. How does Secrets Manager work?
+// It protects your digital secrets like database credentials, API keys, and tokens. It simplifies and strengthens your security and allows you to manage and rotate your secrets.
+
+//! 32. What are some use cases of Secrets Manager?
+// When you want to store your secrets in the most simple and protected ways while easily integrating it in AWS.
+
+//! 33. What is CloudTrail?
+// Auditing, security monitoring, and operational troubleshooting are all made possible via AWS CloudTrail. All user activity and API requests made to AWS services are recorded by CloudTrail as events. Who did what, where, and when? is a question that CloudTrail events assist you in answering.
+
+//! 34. How does CloudTrail work?
+// You have control over storage, analysis, and corrective actions thanks to AWS. CloudTrail's monitoring and recording of account activity across your AWS infrastructure.
+
+//! 35. What are some use cases of CloudTrail?
+//Keep track of, archive, and check the veracity of activity events. Easily create audit reports that are needed for compliance with both internal and external regulations.
+
+//! 36. What is CI/CD integration?
+// A DevOps software development method called continuous integration involves developers routinely merging their code changes into a common repository, which is followed by automated builds and tests.
